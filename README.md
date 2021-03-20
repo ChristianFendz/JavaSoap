ac1341bd4793a59555d4540a8c0fe78ce0f97e38
Christian Ferndz <christianferndz@gmail.com>
2021-03-20 01:26:20 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## SoapJava Java Project
---
+++ src/main/java/com/iciencia/SoapJava/SoapJava/Application.java
@@ -0,0 +1,9 @@
+package com.iciencia.SoapJava.SoapJava;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello SoapJava");
+    }
+
+}
